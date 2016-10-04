a63e4a1741d50e6aa10fbad60d3082d00e5866be
Yarith Joseph <yarithjoseph@gmail.com>
2016-10-05 00:22:23 +0200

    Major changes to all pages

--- Includes/main.js
+++ Includes/main.js
@@ -1,19 +1,22 @@
+$(document).ready()
+
 $("#welcome" ).hide();
-$("#welcome").fadeIn(4000);
-$("#welcome" ).fadeOut(4000);
+$("#welcome").fadeIn(4000)
 
 
 
 $("#welcome2" ).hide();
-$("#welcome2").fadeIn(4000);
+$("#welcome2").fadeIn(7000);
+
+$("#welcome3" ).hide();
+$("#welcome3").fadeIn(10000)
 
 
 
 
 
-$(".content p").hide();
-$(".content p").slideDown(2000);
-$(".content p").show();
+
+
 
 
 
