a4d64309d0e7f2297f441b6d1341fd2cd9472567
Yarith Joseph <yarithjoseph@gmail.com>
2016-10-04 17:04:23 +0200

    added some js

---
+++ Includes/main.js
@@ -0,0 +1,19 @@
+$("#welcome" ).hide();
+$("#welcome").fadeIn(4000);
+$("#welcome" ).fadeOut(4000);
+
+
+
+$("#welcome2" ).hide();
+$("#welcome2").fadeIn(4000);
+
+
+
+
+
+$(".content p").hide();
+$(".content p").slideDown(2000);
+$(".content p").show();
+
+
+
